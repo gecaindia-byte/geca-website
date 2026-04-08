fe4f41f69de223bb4eefe062155fcd071c16060a
gecaindia-byte <gecaindia@gmail.com>
2026-04-08 12:51:43 +0530

    Create tailwind.config.js

---
+++ .github/workflows/tailwind.config.js
@@ -0,0 +1,21 @@
+
+File 2: tailwind.config.js
+Click "Add file" → "Create new file"
+Filename: tailwind.config.js
+Copy-paste this:
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {}
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+}
