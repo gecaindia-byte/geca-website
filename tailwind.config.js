eacaf2d4e9a506616c0c132b59176ef135f1d64c
gecaindia-byte <gecaindia@gmail.com>
2026-04-08 13:10:01 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+}
